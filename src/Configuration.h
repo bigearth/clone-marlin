a8deebdc2dd3ca831755e3d1810e8989e95cb75e
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-06-21 10:17:29 -0700

    Enable EEPROM.

--- src/Configuration.h
+++ src/Configuration.h
@@ -957,7 +957,7 @@ Warning it's loud at the end as the Z axis is still grinding.
 // M501 - reads parameters from EEPROM (if you need reset them after you changed them temporarily).
 // M502 - reverts to the default "factory settings".  You still need to store them in EEPROM afterwards if you want to.
 //define this to enable EEPROM support
-//#define EEPROM_SETTINGS
+#define EEPROM_SETTINGS
 
 #if ENABLED(EEPROM_SETTINGS)
   // To disable EEPROM Serial responses and decrease program space by ~1700 byte: comment this out:
